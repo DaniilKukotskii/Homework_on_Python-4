b3399656331c54094f131b547088e52308409b7f
Daniil <daniilkukotsky@gmail.com>
2023-09-25 00:37:30 +0400

    Changes have been made to the solution function(result)

--- Task2.py
+++ Task2.py
@@ -1,6 +1,6 @@
 
 def user_enter():
-    array_of_berries = []
+    array_of_berriesay_of_berries = []
     i = 1
     while True: 
         try: count_of_bush = int(input("Enter the count of bush: "))
@@ -15,9 +15,9 @@ def user_enter():
             print("Invalid input. Enter only an integer.")
             continue
         else: 
-            array_of_berries.append(count_of_berries)
+            array_of_berriesay_of_berries.append(count_of_berries)
             i += 1
-            if len(array_of_berries) == count_of_bush: break
+            if len(array_of_berriesay_of_berries) == count_of_bush: break
             else: continue
 
     while True:
@@ -27,22 +27,23 @@ def user_enter():
             continue
         else: break
 
-    return array_of_berries, number_of_bush
+    return array_of_berriesay_of_berries, number_of_bush
 
 
 def result(array_of_berries, number_of_bush):
-
-    result = array_of_berries[number_of_bush - 1]     # центральный куст
     
-    if number_of_bush + 1 >= len(array_of_berries): 
-        result += array_of_berries[0]
-        result += array_of_berries[number_of_bush - 2]
-    else: 
-        result += array_of_berries[number_of_bush] 
-        result += array_of_berries[-1]
-        
+    print(array_of_berries)
+    
+    result = 0
+    if number_of_bush == 1:
+        result = array_of_berries[0] + array_of_berries[1] + array_of_berries[-1]
+    elif number_of_bush == len(array_of_berries):
+        result = array_of_berries[-2] + array_of_berries[-1] + array_of_berries[0]
+    else:
+        result = array_of_berries[number_of_bush - 1] + array_of_berries[number_of_bush - 2] + array_of_berries[number_of_bush]
+    
     return result
-        
+
 
 array_of_berries = user_enter()
 print(f"Result: {result(array_of_berries[0], array_of_berries[1])}")
